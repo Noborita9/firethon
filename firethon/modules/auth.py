2ee4f476399715c53199cea8044856bfc84a292b
noborita9 <joaquinbonora9@gmail.com>
2022-10-28 00:08:11 -0300

    test auth

--- firethon/modules/auth.py
+++ firethon/modules/auth.py
@@ -28,3 +28,28 @@ class Auth():
         response = self.request_master.request("sign_in_anonymously", {})
         self.last_user = response
         return response
+
+    def sign_in_with_oauth_credential(self, request_uri, post_body):
+        response = self.request_master.request("sign_in_with_oauth_credentials", {
+                                               "requestUri": request_uri, "postBody": post_body})
+        return response
+
+    def fetch_email_providers(self, identifier, continue_uri):
+        response = self.request_master.request("fetch_email_providers", {
+                                               "identifier": identifier, "continueUri": continue_uri})
+        return response
+
+    def reset_password_with_email(self, email):
+        response = self.request_master.request("reset_password_with_email", {
+                                               "requestType": "PASSWORD_RESET", "email": email})
+        return response
+
+    def verify_password_reset_with_email(self, email):
+        response = self.request_master.request(
+            "verify_password_reset_with_email", {"email": email})
+        return response
+
+    def confirm_password_reset(self, email):
+        response = self.request_master.request(
+            "confirm_password_reset", {"email": email})
+        return response
